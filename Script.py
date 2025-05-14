026b134c506c12b2b87d1709ea8acdc2b90441e9
Sandeepbot1 <vinod974323@gmail.com>
2025-05-14 22:45:27 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -273,7 +273,7 @@ Nᴀᴍᴇ : {}
 
 Mᴇꜱꜱᴀɢᴇ : <b>{}</b>"""
     
-    CAPTION = """<b><a href="https://t.me/dreamcinezone">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/dreamcinezone>[Dreamcine_Botz]</a></b>"""
+    CAPTION = """<b><a href="https://t.me/+lDN-aoJVmzk4YThl">{file_name}</a></b>\n\n<b>⚜️ Powered By : <a https://t.me/+lDN-aoJVmzk4YThl>[Movie_Hobbies]</a></b>"""
 
     IMDB_TEMPLATE_TXT = """
 <b>🏷 Title</b>: <a href={url}>{title}</a>
