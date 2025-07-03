1bb6bcd1ce6ef5f05bfeda20c4a76c1be4f21ad1
Sandeepbot1 <vinod974323@gmail.com>
2025-07-03 20:05:54 +0530

    Update info.py

--- info.py
+++ info.py
@@ -55,8 +55,8 @@ MULTI_FSUB = [int(channel_id) for channel_id in environ.get('MULTI_FSUB', '-1002
 # ============================
 # Payment Configuration
 # ============================
-QR_CODE = environ.get('QR_CODE', 'https://graph.org/file/e419f801841c2ee3db0fc.jpg')
-OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'deendayaldhaked@ybl')
+QR_CODE = environ.get('QR_CODE', 'https://i.ibb.co/XfQMtP6L/photo-2025-07-03-14-04-41-7522856371105038356.jpg')
+OWNER_UPI_ID = environ.get('OWNER_UPI_ID', 'sudeep9743232@axl')
 
 # ============================
 # MongoDB Configuration
@@ -95,7 +95,7 @@ IS_TUTORIAL = bool(environ.get('IS_TUTORIAL', True))
 # ============================
 GRP_LNK = environ.get('GRP_LNK', 'https://t.me/+ar_fv-c6BnUzNmU1')
 CHNL_LNK = environ.get('CHNL_LNK', 'https://t.me/+MdeAR1xBO98xN2U1')
-OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/+Cz1vD1yu4YxhYmI1')
+OWNER_LNK = environ.get('OWNER_LNK', 'https://t.me/Arisu_s_k')
 DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK = environ.get('DEENDAYAL_MOVIE_UPDATE_CHANNEL_LNK', 'https://t.me/+NC3HXVTwHL41OGU1')
 OWNERID = int(os.environ.get('OWNERID', '6662808885'))  # Replace with the actual admin ID
 
